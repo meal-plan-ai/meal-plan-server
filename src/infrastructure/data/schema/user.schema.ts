c30f5ada19b5edc910d9c6d538398e3b8c0a7658
Yevhen <steplerjeka@gmail.com>
2025-01-08 18:26:53 +0200

    add ~/ path resolver for module imports

---
+++ src/infrastructure/data/schema/user.schema.ts
@@ -0,0 +1,32 @@
+import { EntitySchema } from 'typeorm';
+import { User } from '~/domain';
+
+export const UserEntity = new EntitySchema<User>({
+  name: 'Users',
+  target: User,
+  columns: {
+    id: {
+      type: 'uuid',
+      primary: true,
+      generated: 'uuid',
+    },
+    name: {
+      type: 'varchar',
+    },
+    email: {
+      type: 'varchar',
+      unique: true,
+    },
+    password: {
+      type: 'varchar',
+    },
+  },
+  relations: {
+    nutritionPlan: {
+      type: 'one-to-many',
+      target: 'NutritionPlan',
+      inverseSide: '_user',
+      cascade: true,
+    },
+  },
+});
